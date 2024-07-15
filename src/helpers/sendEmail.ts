3f9ecf6d1d3d0cdc57ad9605bb43ba2a8c295e71
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-15 09:26:41 -0600

    chore: Update nodemailer dependency and configure email sending

--- src/helpers/sendEmail.ts
+++ src/helpers/sendEmail.ts
@@ -1,4 +1,23 @@
-import {resend} from '@/lib/resend';
+import { transporter } from "@/lib/nodemailer";
 import verficationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from '@/types/ApiResponse';
+import {render} from '@react-email/components'
 
+export async function sendVerificationEmail(email: string, username: string, verifyCode: string):Promise<ApiResponse> {
+    try {
 
+        const email = render(verficationEmail({username, otp: verifyCode}));
+
+        await transporter.sendMail({
+            from: '3S Universe" <noreply@web-trade.biz>',
+            to: email,
+            subject: 'Verify your email | 3S Universe',
+            html: email
+        })
+      
+        return {success: false, message:"Failed to send the verification email"}
+    } catch (emailError) {
+        console.error("Error sending the verification email", emailError);
+        return {success: false, message:"Failed to send the verification email"}
+    }
+}
---
+++ src/lib/nodemailer.ts
@@ -0,0 +1,12 @@
+import nodemailer from 'nodemailer';
+
+export const transporter = nodemailer.createTransport({
+    host: "live.smtp.mailtrap.io",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.MAILTRAP_USERNAME,
+        pass: process.env.MAILTRAP_PASSWORD
+    }
+});
+

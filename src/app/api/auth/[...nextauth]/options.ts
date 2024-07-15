2c493092da42db58ac7a1426452f8def8432e18f
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-14 22:18:50 -0600

    Add User model

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,3 +1,7 @@
 import { NextAuthOptions } from "next-auth";
 import { CredentialsProvider } from "next-auth/providers/credentials";
 import bcrypt from 'bcrypt';
+import dbConnect from "@/lib/dbConnect";
+import UserModel from '@/models/User';
+
+
---
+++ src/models/User.ts
@@ -0,0 +1,89 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface Message extends Document {
+    senderEmail: string;
+    subject: string;
+    content: string;
+    createdAt: Date;
+}
+
+export const MessageSchema: Schema<Message> = new Schema({
+    senderEmail: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true
+    },
+
+    subject: {
+        type: String,
+        trim: true,
+        required: [true, "Subject is required"]
+    },
+
+    content: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    isVerified: boolean;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    messages: Message[]
+}
+
+export const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "username is required"],
+        trim: true,
+        lowercase: true,
+        unique: true
+    },
+
+    email: {
+        type: String,
+        required: [true, "email is required"],
+        unique: true,
+        lowercase: true,
+        match: [/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g, 'please use a valid email address']
+    },
+
+    password: {
+        type: String,
+        required: [true, "password is required"]
+    },
+
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+
+    verifyCode: {
+        type: String,
+        required: [true, 'verify code is required']
+    },
+
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "verify code expiry is required"]
+    },
+
+    messages: [MessageSchema]
+}, { timestamps: true });
+
+const UserModel = mongoose.models.User as mongoose.Model<User> || mongoose.model<User>("User", UserSchema);
+
+export default UserModel;

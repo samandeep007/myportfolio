0a23e26251f1d62828a3d4a43170f2631fc807f6
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-16 16:25:12 -0600

    chore: Update POST route for replying to messages

--- src/app/api/messages/[id]/route.ts
+++ src/app/api/messages/[id]/route.ts
@@ -31,20 +31,66 @@ export const GET = async(request: Request, {params}: {params: {id: string}}) => 
 export const POST = async(request: Request, {params}: {params: {id: string}}) => {
     await dbConnect();
     try {
-        const session = await getServerSession(authOptions);
-        if(!session || !session){
-            return Response.json({success: false, message: "Unauthorized"},{status: 401})
-        }
-        const userId = session.user._id;
+        // const session = await getServerSession(authOptions);
+        // if(!session || !session){
+        //     return Response.json({success: false, message: "Unauthorized"},{status: 401})
+        // }
+        // const userId = session.user._id;
         const id = params.id;
-        const user = await UserModel.findById(userId);
+        const {reply, username} = await request.json();
+        const user = await UserModel.findOne({username: username});
         if(!user){
             return Response.json({success: false, message: "User not found"}, {status: 404})
         }
+        const message = user.messages.find((message) => String(message._id) === id);
         
+        if(!message){
+            return Response.json({success: false, message: "Message not found!"}, {status: 400});
+        }
+
+        const messageIndex = user.messages.indexOf(message);
+        message.reply = reply;
+        user.messages[messageIndex] = message;
+        
+        await user.save({validateBeforeSave: false});
+
+        return Response.json({success: true, message: "Reply sent successfully"}, {status: 200})
         
     } catch (error) {
         console.error("Error replying to the message");
         return Response.json({success: false, message: "Error replying to the message"}, {status: 500})
     }
 }
+
+
+export const DELETE = async (request: Request, {params}: {params: {id: string}}) => {
+    await dbConnect();
+    const messageId = params.id;
+
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+        return Response.json({success: false, message: "Unauthorized"},{status: 401})
+    }
+    const userId = session.user._id;
+    try {
+       const updateResult =  await UserModel.updateOne(
+            {_id: userId},
+            {
+                $pull: {
+                    messages: {_id: messageId}
+                }
+            }
+        )
+
+        if(updateResult.modifiedCount === 0){
+            return Response.json({success: false, message: "Message not found or already deleted"}, { status: 404 })
+        }
+
+        return Response.json({ success: true, message: "Message deleted successfully!"}, {status: 200});
+           
+
+    } catch (error) {
+        console.log("error in delete message route", error)
+        return Response.json({success: false, message: "Error deleting message!"},{status: 500})
+    }}

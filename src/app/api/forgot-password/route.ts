60683375828399a8f737ef0d496e748696e2af39
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-16 10:44:32 -0600

    chore: Add email verification and password reset functionality

--- src/app/api/forgot-password/route.ts
+++ src/app/api/forgot-password/route.ts
@@ -1,3 +1,4 @@
+import { sendEmail } from "@/helpers/sendEmail";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/models/User";
 
@@ -16,8 +17,14 @@ export const POST = async (request: Request) => {
             return Response.json({success: false, message: "User is not verified. Verify or signUp again"}, {status: 400})
         }
         const verifyCode = String(Math.floor(100000 + Math.random() * 999999));
+        user.verifyCode = verifyCode;
+        await user.save({validateBeforeSave: false});
+        const emailResponse = await sendEmail({email: user.email, username: user.username, verifyCode: verifyCode, type: "PasswordReset", message:"", name:user.fullName});
+        if(!emailResponse.success){
+            return Response.json({success: false, message: "Sending verification email failed"}, {status: 400})
+        }
+        return Response.json({success: true, message: "Password reset email sent"}, {status: 200})
         
-
     } catch (error) {
         console.error("Something went wrong!", error);
         return Response.json({ success: false, message: "Something went wrong" }, { status: 500 });

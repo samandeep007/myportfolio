a360f4459ca8042977a172c40ad64435350778e5
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-18 10:47:09 -0600

    Refactor project routes to remove unused code and fix formatting

--- src/app/api/projects/[id]/route.ts
+++ src/app/api/projects/[id]/route.ts
@@ -1,25 +1,96 @@
-import ProjectModel from "@/models/Project";
+import ProjectModel, { Project } from "@/models/Project";
 import dbConnect from "@/lib/dbConnect";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/options";
+import { uploadOnCloudinary } from "@/helpers/uploadImage";
 
-export const GET = async(request: Request, {params}: {params: {id: string}}) => {
+export const GET = async (request: Request, { params }: { params: { id: string } }) => {
     await dbConnect();
     try {
         const session = await getServerSession(authOptions);
-        if(!session || !session.user){
-            return Response.json({success: false, message: "Unauthorized"},{status:401})
+        if (!session || !session.user) {
+            return Response.json({ success: false, message: "Unauthorized" }, { status: 401 })
         }
         const projectId = params.id;
 
         const project = await ProjectModel.findById(projectId);
-        if(!project){
-            return Response.json({success: false, message: "Project not found!"},{status: 404})
+        if (!project) {
+            return Response.json({ success: false, message: "Project not found!" }, { status: 404 })
         }
-        return Response.json({success: true, message: "Project retrieved successfully!"}, {status: 200});
-        
+        return Response.json({ success: true, message: "Project retrieved successfully!" }, { status: 200 });
+
     } catch (error) {
         console.error("Project retrieval failed", error);
-        return Response.json({success: false, message: "Project retrieval failed"}, {status: 500});
+        return Response.json({ success: false, message: "Project retrieval failed" }, { status: 500 });
+    }
+}
+
+
+export const PUT = async (request: Request, { params }: { params: { id: string } }) => {
+    await dbConnect();
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session || !session.user) {
+            return Response.json({ success: false, message: "Unauthorized" }, { status: 401 })
+        }
+
+        const id = params.id;
+
+        const project = await ProjectModel.findOne({userId: session.user._id, _id: id});
+        if(!project){
+            return Response.json({success: false, message: "Project doesn't exist"}, {status: 404});
+        }
+
+        const formData = await request.formData();
+        const title = formData.get('title') as string || null;
+        const description = formData.get('description') as string || null;
+        const technologies = formData.get('technologies') as string || null;
+        const image = formData.get('image') as File || null;
+
+        const updatedData: any = {};
+        if (image) {
+            const imageBuffer = Buffer.from(await image.arrayBuffer());
+            const imageUrl = await uploadOnCloudinary(imageBuffer);
+            if (imageUrl) {
+                updatedData.image = imageUrl;
+            }
+        }
+
+        if(title) updatedData.title = title;
+        if(description) updatedData.description = description;
+        if(technologies) updatedData.technologies = technologies.split(", ");
+        
+        Object.assign(project, updatedData as Project)
+        await project.save({validateBeforeSave: false});
+        
+        return Response.json({success: true, message: "Project updated successfully"}, {status: 200});
+
+    } catch (error) {
+        console.error("Updating project failed");
+        return Response.json({ success: false, message: "Updating project failed!" }, { status: 500 })
+    }
+}
+
+export const DELETE = async(request: Request, {params}: {params: {id: string}}) =>{
+    await dbConnect();
+    try {
+        const session = await getServerSession(authOptions);
+        if(!session || !session.user){
+            return Response.json({success: false, message: "Unauthorized"}, {status: 401});
+        }
+
+        const id = params.id;
+
+        const deletedProject = await ProjectModel.findOneAndDelete({userId: session.user._id, _id: id});
+
+        if(!deletedProject){
+            return Response.json({success: false, message: "Project doesn't exist"}, {status: 400})
+        }
+
+        return Response.json({success: true, message: "Project deleted successfully"}, {status: 200});
+        
+    } catch (error) {
+        console.error("Deleting project failed!", error);
+        return Response.json({success: false, message: "Deleting project failed"}, {status: 500});
     }
 }
--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -2,6 +2,7 @@ import dbConnect from "@/lib/dbConnect";
 import ProjectModel from "@/models/Project";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/options";
+import { uploadOnCloudinary } from "@/helpers/uploadImage";
 
 
 export const GET = async(request: Request) => {
@@ -21,5 +22,45 @@ export const GET = async(request: Request) => {
     } catch (error) {
         console.error("Retrieving projects failed", error);
         return Response.json({success: false, message: "Retrieving projects failed"},{status:500});
+    }
+}
+
+export const POST = async(request: Request) => {
+    await dbConnect();
+    try {
+        const session = await getServerSession(authOptions);
+        if(!session || !session.user){
+            return Response.json({success: false, message: "Unauthorized"}, {status: 401});
+        }
+        const formData = await request.formData();
+        const title = formData.get('title') as string || null;
+        const description = formData.get('description') as string || null;
+        const link = formData.get('link') as string || null;
+        const technologies = formData.get('technologies') as string || null;
+        const image = formData.get('image') as File || null;
+
+        if([title, description, link, technologies, image].some(field => field === null)){
+            return Response.json({success: false, message: "All fields are required!"}, {status: 400})
+        }
+
+        const imageBuffer = Buffer.from(await image.arrayBuffer());
+        const imageUrl = await uploadOnCloudinary(imageBuffer);
+        if(!imageUrl){
+            return Response.json({success: false, message: "Image upload failed"}, {status: 400});
+        }
+
+        await ProjectModel.create({
+            title: title,
+            description: description,
+            link: link,
+            technologies: technologies?.split(", "),
+            image: imageUrl
+        })
+
+        return Response.json({success: true, message: "Project created successfully"}, {status: 200});       
+        
+    } catch (error) {
+        console.error("Creating project failed", error);
+        return Response.json({success: false, message: "Creating project failed"}, {status: 500});
     }
 }

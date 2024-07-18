3043ecc3d830d32a1b78485543eae6992849d6ce
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-17 22:11:40 -0600

    chore: Refactor project model and schema

---
+++ src/models/Project.ts
@@ -0,0 +1,63 @@
+import mongoose, {Schema, Document} from 'mongoose';
+
+export interface Feature extends Document {
+    title: string;
+    description: string;
+    media: string;
+}
+
+const featureSchema:Schema<Feature> = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    media: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+
+export interface Project extends Document {
+    title: string;
+    description: string;
+    features: [Feature];
+    link: string;
+    technologies: Array<String>
+    image: string;
+}
+
+const projectSchema: Schema<Project> = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    features: [featureSchema],
+    link: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    technologies: [String],
+    image: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, {timestamps: true})
+
+export const ProjectModel = mongoose.models.Project as mongoose.Model<Project> || mongoose.model('Project', projectSchema);

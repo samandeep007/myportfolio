1d14514816bdbc2d6dc99324b98cb4d1743c98b4
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-19 13:50:01 -0600

    chore: Add Education model and schema

---
+++ src/models/Education.ts
@@ -0,0 +1,73 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IEducation extends Document {
+    userId: string;
+    school: string;
+    degree: string;
+    field: string;
+    startDate: Date;
+    endDate: Date;
+    grade: Number;
+    Activities: string;
+    description: string;
+    skills: Array<String>;
+    media: string;
+}
+
+const EducationSchema: Schema<IEducation> = new Schema({
+    userId: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    school: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    degree: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    field: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    grade: {
+        type: Number,
+        required: true
+    },
+    Activities: {
+        type: String,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    skills: {
+        type: [String],
+        required: true,
+        default: []
+    },
+    media: {
+        type: String,
+        trim: true
+    }
+}, { timestamps: true });
+
+
+const EducationModel = mongoose.models.Education as mongoose.Model<IEducation> || mongoose.model('Education', EducationSchema);
+export default EducationModel;

6e2935f1548d3aac8277cd627bdf79867a10de54
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-20 12:09:52 -0600

    chore: Update PUT API route for updating education details with media upload support

--- src/app/api/education/[id]/route.ts
+++ src/app/api/education/[id]/route.ts
@@ -2,6 +2,7 @@ import EducationModel from "@/models/Education";
 import dbConnect from "@/lib/dbConnect";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/options";
+import { uploadOnCloudinary } from "@/helpers/uploadImage";
 
 export const GET = async (request: Request, { params }: { params: { id: string } }) => {
     await dbConnect();
@@ -20,6 +21,58 @@ export const GET = async (request: Request, { params }: { params: { id: string }
     } catch (error) {
         console.error("Failed to retrieve education details", error);
         return Response.json({ success: false, message: "Failed to retrieve education details" }, { status: 500 });
+    }
+}
+
+export const PUT = async(request: Request, {params}: {params: {id: string}}) => {
+    await dbConnect();
+    try {
+        const session = await getServerSession(authOptions);
+        if(!session || !session.user){
+            return Response.json({success: false, message: "Unauthorized"}, {status: 401});
+        }
+        const userId = session.user._id;
+        const id = params.id;
+        const education = await EducationModel.findOne({userId: userId, _id: id});
+        if(!education){
+            return Response.json({success: false, message: "Education details not found"}, {status: 400});
+        }
+        const formData = await request.formData();
+        const school = formData.get('school') as string || null;
+        const degree = formData.get('degree') as string || null;
+        const field = formData.get('field') as string || null;
+        const startDate = formData.get('startDate') as string || null;
+        const endDate = formData.get('endDate') as string || null;
+        const grade = formData.get('grade') as string || null;
+        const activities = formData.get('activities') as string || null;
+        const description = formData.get('description') as string || null;
+        const skills = formData.get('skills') as string || null;
+        const media = formData.get('media') as File || null;
+
+        if(media){
+            const mediaBuffer = Buffer.from(await media.arrayBuffer());
+            const mediaUrl = await uploadOnCloudinary(mediaBuffer);
+            if(!mediaUrl){
+                return Response.json({success: false, message: "Media upload failed!"}, {status: 400});
+            }
+            education.media = mediaUrl;
+        }
+
+        if(school) education.school = school;
+        if(degree) education.degree = degree;
+        if(field) education.field = field;
+        if(startDate) education.startDate = new Date(startDate);
+        if(endDate) education.endDate = new Date(endDate);
+        if(grade) education.grade = Number(grade);
+        if(activities) education.activities = activities;
+        if(description) education.description = description;
+        if(skills) education.skills = skills.split(', ');
+
+        await education.save({validateBeforeSave: false});
+        return Response.json({success: true, message: "Education details updated successfully"}, {status: 200});
+    } catch (error) {
+        console.error("Failed to update education details", error);
+        return Response.json({success: false, message: "Failed to delete education details"}, {status: 500});
     }
 }
 

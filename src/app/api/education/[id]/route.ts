ff9bc197c62930a9e56153eaaf3161fb5b4fc930
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-20 11:56:35 -0600

    chore: Add DELETE API route for deleting education details

--- src/app/api/education/[id]/route.ts
+++ src/app/api/education/[id]/route.ts
@@ -22,3 +22,25 @@ export const GET = async (request: Request, { params }: { params: { id: string }
         return Response.json({ success: false, message: "Failed to retrieve education details" }, { status: 500 });
     }
 }
+
+
+export const DELETE = async (request: Request, { params }: { params: { id: string } }) => {
+    await dbConnect();
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session || !session.user) {
+            return Response.json({ success: false, message: "Unauthorized" }, { status: 401 });
+        }
+        const userId = session.user._id;
+        const id = params.id;
+        const deletedEducation = await EducationModel.findOneAndDelete({ userId: userId, _id: id });
+        if (!deletedEducation) {
+            return Response.json({ success: false, message: "Education details not found" }, { status: 404 });
+        }
+        return Response.json({ success: true, message: "Education details deleted successfully" }, { status: 200 });
+
+    } catch (error) {
+        console.error("Failed to delete education details", error);
+        return Response.json({ success: false, message: "Failed to delete education details" }, { status: 500 });
+    }
+}

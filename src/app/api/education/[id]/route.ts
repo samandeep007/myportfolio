ab2a7df12a7380fbff29c0cc557bcd4105d60c26
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-20 11:51:46 -0600

    chore: Update API route for retrieving education details and add authentication

--- src/app/api/education/[id]/route.ts
+++ src/app/api/education/[id]/route.ts
@@ -3,6 +3,22 @@ import dbConnect from "@/lib/dbConnect";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/options";
 
-export const GET = async(request: Request, {params}: {params: {id: string}}) => {
-    
+export const GET = async (request: Request, { params }: { params: { id: string } }) => {
+    await dbConnect();
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session || !session.user) {
+            return Response.json({ success: false, message: "Unauthorized" }, { status: 401 });
+        }
+        const userId = session.user._id;
+        const id = params.id;
+        const education = await EducationModel.findOne({ userId: userId, _id: id });
+        if (!education) {
+            return Response.json({ success: false, message: "Education details not found" }, { status: 404 });
+        }
+        return Response.json({ success: true, message: "Education details retrieved successfully", data: education }, { status: 200 });
+    } catch (error) {
+        console.error("Failed to retrieve education details", error);
+        return Response.json({ success: false, message: "Failed to retrieve education details" }, { status: 500 });
+    }
 }
